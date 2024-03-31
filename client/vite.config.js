c237367ace1ea8cb3e02f101975b869c92b42768
Rohit <93265718+rohitarrunachalam@users.noreply.github.com>
2024-03-31 20:34:21 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5000/api",
+        target: "https://postcareai-gpt.onrender.com/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
